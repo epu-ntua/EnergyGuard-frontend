3bbddf4b4febc498155872bd5b865c63080ac1f1
stavpb <stavroula141@gmail.com>
2025-09-25 13:27:37 +0300

    feat: add experiments_list HTML with view and url mapping

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('experiments-list/', views.experiments_list, name='experiments_list'),
+
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render 
+from datetime import datetime
+import mlflow
 
 def home(request):
     return render(request, 'mysite/home.html', {})
+
+def experiments_list(request):
+
+    mlflow.set_tracking_uri("https://mlflow.toolbox.epu.ntua.gr/")
+    
+    data = []
+    for exp in mlflow.search_experiments():
+        created = datetime.fromtimestamp(exp.creation_time / 1000.0).date() if exp.creation_time else None
+        updated = datetime.fromtimestamp(exp.last_update_time / 1000.0).date() if exp.last_update_time else None
+
+        data.append({
+            "name": exp.name,
+            "start_date": created,
+            "last_update": updated,
+            "status": exp.lifecycle_stage,
+            "artifact_uri": exp.artifact_location
+        })
+
+    return render(request, 'mysite/experiments_list.html', {"experiments" : data})
