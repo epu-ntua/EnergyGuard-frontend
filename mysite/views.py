2afd7a353add122691466f504ebeabb082d937c1
stavpb <stavroula141@gmail.com>
2025-10-03 17:21:37 +0300

    feat(mysite): update dataset_list and views to set active state dynamically

--- mysite/views.py
+++ mysite/views.py
@@ -71,4 +71,4 @@ def datasets_list(request):
     page_number = request.GET.get("page")
     filtered_data = p.get_page(page_number)
 
-    return render(request, 'mysite/dataset_list.html', {"dataset" : filtered_data, "dataset_num": counts, "active_navbar_page": "datasets"})
+    return render(request, 'mysite/dataset_list.html', {"dataset" : filtered_data, "dataset_num": counts, "status_filter": status_filter, "active_navbar_page": "datasets"})
