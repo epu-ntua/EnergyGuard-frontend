b3a4e974bafc0b33db195dfc7fe8cd844bbaafb4
stavpb <stavroula141@gmail.com>
2025-10-06 12:34:13 +0300

    feat(pagination): add reusable Django inclusion tag for smart pagination with ellipsis

---
+++ mysite/templatetags/pagination_tags.py
@@ -0,0 +1,33 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('mysite/pagination.html')
+def smart_pagination(page_obj, adjacent_pages=1):
+    """
+    Inclusion tag: returns context with pagination data
+    """
+    total_pages = page_obj.paginator.num_pages
+    current = page_obj.number
+    pages = []
+
+    for i in range(1, total_pages + 1):
+        if (
+            i == 1 or
+            i == total_pages or
+            (i >= current - adjacent_pages and i <= current + adjacent_pages)
+        ):
+            pages.append(i)
+
+    display = []
+    last = 0
+    for p in pages:
+        if p - last > 1:
+            display.append('...')
+        display.append(p)
+        last = p
+
+    return {
+        'page_obj': page_obj,
+        'page_list': display,
+    }
