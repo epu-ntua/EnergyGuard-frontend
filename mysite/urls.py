fbc29a6754d36af498b6d3617afc929c71ada637
stavpb <stavroula141@gmail.com>
2025-10-17 17:28:25 +0300

    chore(urls): add routes for registration wizard and success page

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('experiments-list/', views.experiments_list, name='experiments_list'),
     path('datasets-list/', views.datasets_list, name='datasets_list'),
     path('billing/', views.billing, name='billing'),  # New billing path
-    path('register/', views.register, name='register'),
+    # path('register/', views.register, name='register'),
+    path('register/', views.RegistrationWizard.as_view(views.FORMS), name='register'),
+    path('registration-success/', views.registration_success, name='registration_success')
 ]
