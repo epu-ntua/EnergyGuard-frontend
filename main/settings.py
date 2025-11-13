b65a35bee800fa4fbbbcc637839ad69c1e8cfb98
stavpb <stavroula141@gmail.com>
2025-11-13 19:36:44 +0200

    fix(settings): use variable for SECRET_KEY instead of the actual key

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qxeyne5tp#7(6fxp$8so32etapq^9!(16_n^qz19c)m0r8qax@'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
