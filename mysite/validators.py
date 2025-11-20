bd1c9e08165c9f05927a8499a67a24fd01ddd085
stavpb <stavroula141@gmail.com>
2025-11-20 15:47:11 +0200

    chore(validators): remove username validator since username is no longer used

--- mysite/validators.py
+++ mysite/validators.py
@@ -4,8 +4,3 @@ strict_email_user_validator = RegexValidator(
     r'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
     'Enter a valid email user. Only letters, numbers, and . - _ are allowed before the @ symbol.'
 )
-
-strict_username_validator = RegexValidator(
-    r'^[a-zA-Z0-9_.-]+$',
-    'Username can only contain letters, numbers, and . - _ characters.'
-)
