1d4b62c10451e83c2832a3b1a788e3b846806af1
stavpb <stavroula141@gmail.com>
2026-01-12 22:17:01 +0200

    feat(admin): register models in django admin

--- experiments/admin.py
+++ experiments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Experiment, ExperimentCollaborator
 
 # Register your models here.
+admin.site.register(Experiment)
+admin.site.register(ExperimentCollaborator)
