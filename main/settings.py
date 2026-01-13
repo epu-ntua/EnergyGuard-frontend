ba5fe4a9a07e17ae16eee64f0909c80e50c0cd2c
stavpb <stavroula141@gmail.com>
2026-01-13 12:53:18 +0200

    fix(settings): correct STATICFILES_DIRS

--- main/settings.py
+++ main/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles' # Directory where static files will be collected (for Docker and production)
 
