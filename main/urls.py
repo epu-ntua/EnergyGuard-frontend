57ef20b5683a7794c352586d56ff4bc46a1931cb
stavpb <stavroula141@gmail.com>
2026-01-21 16:55:38 +0200

    fix(avatar): correct user avatar display

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('assets/', include('assets.urls')),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
