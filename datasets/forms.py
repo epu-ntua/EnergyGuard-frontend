4f2be04e750fd9a0ddbb17b40a182e51c85050a2
stavpb <stavroula141@gmail.com>
2026-01-23 17:19:29 +0200

    feat(forms): control data_file format for dataset upload

--- datasets/forms.py
+++ datasets/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 from datasets.models import Dataset
 
 class GeneralDatasetForm(forms.ModelForm):
@@ -27,9 +29,23 @@ class MetadataDatasetForm(forms.ModelForm):
         }
 
 class FileUploadDatasetForm(forms.ModelForm):
+    data_file = forms.FileField(
+        validators=[FileExtensionValidator(allowed_extensions=['zip', 'csv'])],
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'}),
+    )
+
     class Meta:
         model = Dataset
         fields = ('data_file',)
         widgets = {
             'data_file': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
+
+    def clean_data_file(self):
+        uploaded = self.cleaned_data.get('data_file')
+        if not uploaded:
+            return uploaded
+        allowed_types = {'application/zip', 'application/x-zip-compressed', 'text/csv', 'application/csv'}
+        if uploaded.content_type not in allowed_types:
+            raise ValidationError("Only .zip or .csv files are allowed.")
+        return uploaded
