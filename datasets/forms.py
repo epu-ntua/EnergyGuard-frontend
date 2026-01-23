c668c89b3077d32134878f9cd04117e810da3d97
stavpb <stavroula141@gmail.com>
2026-01-23 12:19:03 +0200

    feat(forms): add dataset forms for dataset upload wizard

---
+++ datasets/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from datasets.models import Dataset
+
+class GeneralDatasetForm(forms.ModelForm):
+    class Meta:
+        model = Dataset
+        fields = ('name', 'description', 'label', 'visibility')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter dataset name'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter dataset description', 'rows': 2}),
+            'label': forms.Select(attrs={'class': 'form-select'}),
+            'visibility': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class MetadataDatasetForm(forms.ModelForm):
+    class Meta:
+        model = Dataset
+        fields = ('metadata',)
+        widgets = {
+            'metadata': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter dataset metadata in JSON format', 'rows': 5}),
+        }
+
+class FileUploadDatasetForm(forms.ModelForm):
+    class Meta:
+        model = Dataset
+        fields = ('data_file',)
+        widgets = {
+            'data_file': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
