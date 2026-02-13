027fbad4d07982e445dd48204c3e9ee436f62e89
stavpb <stavroula141@gmail.com>
2026-02-13 17:38:35 +0200

    fix(urls): correct path for dataset download

--- datasets/urls.py
+++ datasets/urls.py
@@ -5,8 +5,7 @@ urlpatterns = [
     path('', datasets_list, name='datasets_list'),
     path('data/', DatasetsListJson.as_view(), name='datasets_list_json'),
     path('dataset/<int:dataset_id>/', dataset_details, name='dataset_details'),
-    path('dataset/<int:dataset_id>/download/', dataset_details, name='dataset_download'),
+    path('dataset/<int:dataset_id>/download/', dataset_download, name='dataset_download'),
     path('dataset-upload/', AddDatasetView.as_view(DATASET_FORMS), name='dataset_upload'),
     path('dataset-upload-success/', dataset_upload_success, name='dataset-upload-success'),
-
 ]
