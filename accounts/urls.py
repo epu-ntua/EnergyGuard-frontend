8b2c0091c8a0367580c42ca3ca12b1a58d94dc00
stavpb <stavroula141@gmail.com>
2026-03-02 18:09:15 +0200

    chore(accounts): remove unused platform-entry wizard and related templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,10 @@ from accounts import views
 urlpatterns = [
     path('register/', views.RegistrationWizard.as_view(views.REGISTRATION_FORMS), name='register'),
     path('platform-registration-success/', views.platform_registration_success, name='platform_registration_success'),
-    path('registration-success/', views.keycloak_registration_success, name='keycloak_registration_success'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.keycloak_logout, name='account_logout'),
     path('profile/', views.profile, name='profile'),
     # path('keycloak-redirect/', views.keycloak_redirect, name='account_keycloak_redirect'),
-    path('platform-entry/', views.PlatformEntryView.as_view(views.ENTRY_FORMS), name='platform_entry'),
     path('profile/update-picture/', views.update_profile_picture, name='update_profile_picture'),
     path('team-management/', views.team_management, name='team_management'),
 ]
