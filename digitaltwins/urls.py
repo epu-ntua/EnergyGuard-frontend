f37efd97837e09852bb6bf62b854fee1888cb52f
stavpb <stavroula141@gmail.com>
2026-06-16 20:00:53 +0300

    style(templates): standardize content padding, margin across pages, add breadcrumb and fix map viewport

--- digitaltwins/urls.py
+++ digitaltwins/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.map_view, name='digitaltwins-map'),
+    path('', views.map_view, name='riga-map'),
     path('list/', views.digitaltwins_list, name='digitaltwins-list'),
     path('<slug:slug>/', views.digitaltwins_detail, name='digitaltwins-detail'),
 ]
