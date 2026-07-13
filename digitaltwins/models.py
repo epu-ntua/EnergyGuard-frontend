dc4b3519f86a3dad9855591557dbef2fcdd1b005
stavpb <stavroula141@gmail.com>
2026-07-13 17:44:00 +0300

    feat(digitaltwins): add dt_result table to db

--- digitaltwins/models.py
+++ digitaltwins/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class DtResult(TimeStampedModel):
+    twin_slug = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bucket_name = models.CharField(max_length=100, default='')
+    result_key = models.CharField(max_length=1024)
+
+    class Meta:
+        db_table = 'dt_result'
+        verbose_name = 'DT Result'
+        verbose_name_plural = 'DT Results'
